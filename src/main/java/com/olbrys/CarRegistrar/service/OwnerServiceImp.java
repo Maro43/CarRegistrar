9e302acfcf496b10df919e15758a31396e7bfee1
Maro 43 <mariusz.olbrys@outlook.com>
2023-08-14 10:35:15 +0200

    Owner Update service

--- src/main/java/com/olbrys/CarRegistrar/service/OwnerServiceImp.java
+++ src/main/java/com/olbrys/CarRegistrar/service/OwnerServiceImp.java
@@ -34,7 +34,11 @@ public class OwnerServiceImp implements OwnerService {
     @Override
     public OwnerDto updateOwner(OwnerDto ownerDto, Long id) {
         OwnerEntity ownerEntity = ownerRepository.findById(id).orElse(null);
-        return null;
+        ownerEntity.setFirstName(ownerDto.getFirstName());
+        ownerEntity.setLastName(ownerDto.getLastName());
+        ownerEntity.setValidLicence(ownerDto.isValidLicence());
+        OwnerEntity save = ownerRepository.save(ownerEntity);
+        return new OwnerDto(save);
     }
 
     @Override
