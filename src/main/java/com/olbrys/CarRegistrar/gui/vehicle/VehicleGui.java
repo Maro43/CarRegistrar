1fe2e32f09633b62fbb0d5f2782b7abdd131ec49
Maro 43 <mariusz.olbrys@outlook.com>
2023-08-31 10:48:14 +0200

    Prepare Vehicle service and Gui

--- src/main/java/com/olbrys/CarRegistrar/gui/vehicle/VehicleGui.java
+++ src/main/java/com/olbrys/CarRegistrar/gui/vehicle/VehicleGui.java
@@ -2,13 +2,10 @@ package com.olbrys.CarRegistrar.gui.vehicle;
 
 import com.vaadin.flow.component.html.Label;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 
-@Route
+@Route("/Vehicle")
 public class VehicleGui extends VerticalLayout {
-
-    private TextField testField;
 
     public VehicleGui(){
         Label label = new Label("Menu opcji Pojazdów");
